92124742c0789c8e4ce59547ea160f37ac6b8a5c
Mohammed Osman <mohammed.osman@vianeos.com>
2025-11-07 17:08:50 +0400

    sync clerk auth with my DB using inngest.

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -7,4 +7,12 @@ export const ENV = {
   DATABASE_URL: process.env.DATABASE_URL || "",
   SECRET_KEY: process.env.SECRET_KEY || "",
   ENV: process.env.ENV || "development",
+
+  CLIENT_URL: process.env.CLIENT_URL || "http://localhost:5173",
+
+  INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+  INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
+
+  STREAM_API_KEY: process.env.STREAM_API_KEY,
+  STREAM_API_SECRET: process.env.STREAM_API_SECRET,
 };
---
+++ backend/src/lib/inngest.js
@@ -0,0 +1,48 @@
+import inngest from "inngest";
+import { connectDB } from "./db.js";
+import { User } from "../models/User.js";
+
+export const inngestClient = new inngest.Inngest({ id: "talent-iq" });
+
+const syncUser = inngestClient.createFunction(
+  { id: "sync-user", event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
+
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+
+    // Check if user already exists
+    let user = await User.findOne({ clerckId });
+
+    if (!user) {
+      // Create new user
+      const newUser = {
+        name: `${first_name || ""} ${last_name || ""}`.trim(),
+        email: email_addresses[0]?.email_address || "",
+        profileImage: image_url || "",
+        clerckId: id,
+      };
+      user = new User(newUser);
+      await user.save();
+    }
+
+    return user;
+  }
+);
+
+const deleteUser = inngestClient.createFunction(
+  { id: "delete-user", event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
+
+    const { id } = event.data;
+
+    // Delete user
+    await User.findOneAndDelete({ clerckId: id });
+
+    return { success: true };
+  }
+);
+
+export const inngestFunctions = [syncUser];
